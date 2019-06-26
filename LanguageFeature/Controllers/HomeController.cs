7eaa33d4345d6eee44f7742d2f77bd4e7d6f67e1
btiwari <Bhupentiwari@live.com>
2019-06-26 13:13:41 +0200

    Learn Lanugae feature Property

--- LanguageFeature/Controllers/HomeController.cs
+++ LanguageFeature/Controllers/HomeController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using LanguageFeature.Models;
 namespace LanguageFeature.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public string Index()
         {
-            return View();
+            return "Hello , We are going to learn various language features";
+        }
+
+        public ViewResult AutoProperty()
+        {
+            Product product = new Product();
+            product.Name = "Redmi Note 5 Pro";
+            string productName = product.Name;
+            /*
+             this is because the View method has an overload that accepts two String arguments and which has a different meaning to the overload that accepts a String and an object. 
+             to avoid calling the wrong one, i explicitly cast the second argument
+             */
+            return View("Result", (object)string.Format("Product name is {0}", product.Name));
         }
     }
 }
---
+++ LanguageFeature/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguageFeature.Models
+{
+    public class Product
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
